e984109b412d69c6f13226f1faa8c7c6a5f367b3
githubrajesh <42956898+githubrajesh@users.noreply.github.com>
2018-12-12 19:02:06 +0530

    js file uploaded

---
+++ sample.js
@@ -0,0 +1,18 @@
+describe("zebpay website login", function() {
+  it('should add a todo', function() {
+	
+	browser.ignoreSynchronization = true;
+	
+	browser.driver  
+	browser.driver.manage().window().maximize();
+    browser.get('https://pro.zebpay.co/');
+	
+	element(by.id('contactNumber')).sendKeys('7400084561');
+	element(by.id('sendMsg')).click;
+	
+	element(by.n	
+	
+	
+  });
+    
+});
